9f7b184965be29cf35057aa515159d983ea7505c
Bashe Abdirahman <basheabdi@gmail.com>
2018-04-14 14:07:28 -0700

    First Try used to test Jenkins Pipeline

---
+++ JenkinsProject_NUNit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JenkinsProject_NUNit.MvcApplication" Language="C#" %>
---
+++ JenkinsProject_NUNit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JenkinsProject_NUNit.Startup))]
+namespace JenkinsProject_NUNit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
